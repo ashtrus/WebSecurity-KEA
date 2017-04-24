86cfb86790dbc127a6c077fd01db9d40359c8356
LogicAppSourceIO <logicappsource@gmail.com>
2017-04-24 22:34:18 +0200

    PDO, OOP, USER CLASS, ENC PASS, SIGNUP, LOGIN

--- petlovers/index.php
+++ petlovers/index.php
@@ -1,3 +1,31 @@
+<?php 
+require_once ('./functions/dbconfig.php');
+
+if($user->is_loggedin()!="")
+{
+ $user->redirect('home.php');
+}
+
+if(isset($_POST['btn-login']))
+{
+ $uname = $_POST['txt_uname_email'];
+ $umail = $_POST['txt_uname_email'];
+ $upass = $_POST['txt_password'];
+  
+ if($user->login($uname,$umail,$upass))
+ {
+  $user->redirect('home.php');
+  echo 'correct';
+ }
+ else
+ {
+  $error = "Wrong Details !";
+  echo $error;
+ } 
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -251,7 +279,6 @@
     </footer>
 
     <!-- Login Modal -->
-
     <div class="modal fade" id="modalLogin">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
@@ -262,22 +289,56 @@
             </button>
         </div>
         <div class="modal-body">
+            <!-- NEW FORM --> 
+            <div class="container">
+     <div class="form-container">
+        <form method="post">
+            <h2>Sign in.</h2><hr />
+            <?php
+            if(isset($error))
+            {
+                  ?>
+                  <div class="alert alert-danger">
+                      <i class="glyphicon glyphicon-warning-sign"></i> &nbsp; <?php echo $error; ?> !
+                  </div>
+                  <?php
+            }
+            ?>
+            <div class="form-group">
+             <input type="text" class="form-control" name="txt_uname_email" placeholder="Username or E mail ID" required />
+            </div>
+            <div class="form-group">
+             <input type="password" class="form-control" name="txt_password" placeholder="Your Password" required />
+            </div>
+            <div class="clearfix"></div><hr />
+            <div class="form-group">
+             <button type="submit" name="btn-login" class="btn btn-block btn-primary">
+                 <i class="glyphicon glyphicon-log-in"></i>&nbsp;SIGN INNew
+                </button>
+            </div>
+            <br />
+            <label>Don't have account yet ! <a href="functions/sign-up.php">Sign Up</a></label>
+        </form>
+       </div>
+</div>
+
+            <!--
             <form class="form-horizontal">
                 <div class="form-group">
                     <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
                     <div class="col-sm-10">
-                    <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                    <input type="email" class="form-control" id="inputemail" placeholder="Email">
                     </div>
                 </div>
                 <div class="form-group">
                     <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
                     <div class="col-sm-10">
-                    <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                    <input type="password" class="form-control" id="inputpassword" placeholder="Password">
                     </div>
                 </div>
                 <div class="form-group">
                     <div class="col-sm-offset-2 col-sm-2">
-                    <button type="submit" class="btn btn-default">Log in</button>
+                    <button type="submit" id="login-btn" class="btn btn-default">Log in</button>
                     </div>
                     <div class="col-sm-2">
                     <button id="openModalRegister" class="btn btn-default btn-danger">Register</button>
@@ -290,11 +351,11 @@
         </div>
         </div>
     </div>
+    -->
     </div>
 
 
     <!-- Register Modal -->
-
     <div class="modal fade" id="modalRegister">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
@@ -305,7 +366,10 @@
             </button>
         </div>
         <div class="modal-body">
-            <form class="form-horizontal">
+           
+           <?php include '/functions/Sign-Up.php'; ?>
+
+           <!-- <form class="form-horizontal">
                 <div class="form-group">
                     <label for="txtRegName" class="col-sm-2 control-label">Name</label>
                     <div class="col-sm-10">
@@ -319,7 +383,7 @@
                     </div>
                 </div>
                 <div class="form-group">
-                    <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+                    <label for="inputEmail" class="col-sm-2 control-label">Email</label>
                     <div class="col-sm-10">
                     <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
                     </div>
@@ -335,7 +399,7 @@
                     <button class="btn btn-default btn-danger">Register</button>
                     </div>
                 </div>
-            </form>
+            </form>-->
         </div>
         <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -344,13 +408,23 @@
     </div>
     </div>
 
-
+        
 
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
+
+
+        <!--Auth --> 
+        <script src="js/auth.js"></script>
+
+
+
+
+
+
 
     <!-- Custom Theme JavaScript -->
     <script>
---
+++ petlovers/js/auth.js
@@ -0,0 +1,32 @@
+
+//Login 
+
+$('#login-btn').on('click', function() {
+    var email = $('#inputemail').val();   
+    var password = $('#inputpassword').val();   
+
+var sLink = "./functions/login.php?email="+email+"&password="+password; 
+console.log(sLink);
+
+$.ajax({
+    "method":"GET",
+    "url": sLink,
+    "dataType": "JSON"
+    
+}).done(function(jData) {
+    if(jData.status == 'ok'){
+        console.log("success");
+    } else {
+        console.log("failed");
+    }
+    
+    }).fail(function(jFail) {
+       
+    })
+
+
+})
+
+
+
+//Register 
