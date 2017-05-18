0d32ef2fd2227f764b0ff8cbeb4666cbc8d0e322
ashtrus <zygimantas.butas@gmail.com>
2017-05-18 19:09:43 +0200

    Menu fixed

--- petlovers/user-profile.php
+++ petlovers/user-profile.php
@@ -23,55 +23,8 @@ require_once ('./functions/dbconfig.php');
     */
 
     if ( isset($_SESSION['user_session']) ) {
-	echo esc($_SESSION['user_session']);
-}
+      echo esc($_SESSION['user_session']);
+    }
 ?>
 
-<?php include_once 'components/footer.php'; ?> 
-
-
-
-
-<script> 
-
-  function storeLocalStorage() {
-
-    //Global user 
-    //verify user with id or token backend 
-    //object to -> stringify 
-    //userName , user email , token, store in lcoal storage. 
-   
-  }
-
-    var sLink = "functions/api/get-users.php"; 
-
-    $.ajax({
-      "url": sLink,
-      "method": "GET",
-      "dataType": "JSON",
-      "cache": false 
-
-    }).done(function(jData) {
-       // console.log( jData); 
-
-        $.each(jData, function(i,jObjectData) {
-                  console.log(jObjectData );
-
-            var sUsername = jObjectData.user_name; 
-                  console.log("My user name is ",  sUsername);
-
-            var sUseremail = jObjectData.user_email; 
-                  console.log("My user Email is " , sUseremail);
-
-            var sUserid  = jObjectData.user_id; 
-                  console.log('My user id is ', sUserid);
-
-            var sUserAuthToken = jObjectData.user_token; 
-                  console.log("My user Token is " , sUserAuthToken);
-        });
-
-    }).fail(function(jFail) {
-        console.log('Failed');
-    })
-
-</script> 
+<?php include_once 'components/footer.php'; ?>
